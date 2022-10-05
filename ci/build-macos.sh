1181220972404fbed61e52d48a0d48f610b5a8ac
Akshob Gopalakrishna <info@akshob.in>
2022-10-04 23:23:03 -0700

    hmm I dont really need brew

--- ci/build-macos.sh
+++ ci/build-macos.sh
@@ -4,23 +4,7 @@ set -euo pipefail
 # You can run it from any directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-set_homebrew_prefix() {
-    if [ -f "/usr/local/bin/brew" ]; then
-        export HOMEBREW_PREFIX="/usr/local"
-    elif [ -f "/opt/homebrew/bin/brew" ]; then
-        export HOMEBREW_PREFIX="/opt/homebrew"
-    fi
-}
-
-set_homebrew_prefix
-
-eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
-
-brew install akshob/tools/dotfiles-init
-dotfiles-init
+$DIR/dotfiles-init .
 shellrc=."$(echo $SHELL | cut -d '/' -f3)"rc
 source $shellrc
 ll
